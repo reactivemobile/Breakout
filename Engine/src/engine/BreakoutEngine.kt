788806bd3c172ba10dd84d7051e4cb630ba26f72
Donal OCallaghan <donal@reactivemobile.com>
2018-03-05 22:07:52 +0000

    Basic block implementation

--- Engine/src/engine/BreakoutEngine.kt
+++ Engine/src/engine/BreakoutEngine.kt
@@ -1,20 +1,29 @@
 package engine
 
-class BreakoutEngine(var width: Double, var height: Double, val ballRadius: Double, val paddleWidth: Double, val paddleHeight: Double, val blockWidth: Double, val blockHeight: Double, val numLives: Int, val gameStateListener: GameStateListener) {
-    internal var ball: Ball = Ball(width / 2, height / 2, 1.0, 1.0, width, height, ballRadius)
+class BreakoutEngine(var width: Double, var height: Double, val ballRadius: Double, val paddleWidth: Double, val paddleHeight: Double, val blockColumns: Int, val blockRows: Int, val numLives: Int, val gameStateListener: GameStateListener) {
+    internal var ball: Ball = Ball(width / 2, height / 2, 1.0, 1.0, ballRadius)
     var paddleX: Double = 0.0
     var paddleY: Double = height - paddleHeight
+
+    val blockWidth = width / blockColumns
+    val blockHeight = (width / 2) / blockRows
+    var blocks: Array<Block> = Array(blockColumns * blockRows, { it -> Block((it % blockColumns).toDouble() * blockWidth, (it / blockColumns).toDouble() * blockHeight, blockWidth, blockHeight, BlockState.NEW) })
 
     var running = true
     var lives = numLives
     fun resetBall() {
         ball.x = width / 2
-        ball.y = height / 2
+        ball.y = width / 2
     }
 
     fun resetGame() {
         resetBall()
         resetLives()
+        resetBlocks()
+    }
+
+    private fun resetBlocks() {
+
     }
 
     private fun resetLives() {
@@ -34,6 +43,15 @@ class BreakoutEngine(var width: Double, var height: Double, val ballRadius: Doub
         if (running) {
             gameStateListener.ballMoved(ball.x, ball.y)
             ball.step()
+            stepBlocks()
+        }
+    }
+
+    private fun stepBlocks() {
+        for (block in blocks) {
+            println(block.toString())
+            block.checkHit(ball)
+            gameStateListener.blockUpdated(block)
         }
     }
 
@@ -44,18 +62,18 @@ class BreakoutEngine(var width: Double, var height: Double, val ballRadius: Doub
         }
     }
 
-    inner class Ball(var x: Double, var y: Double, var velocityX: Double, var velocityY: Double, val width: Double, val height: Double, val radius: Double) {
+    inner class Ball(var ballX: Double, var ballY: Double, var velocityX: Double, var velocityY: Double, val radius: Double) : Rectangle(ballX, ballY, radius * 2, radius * 2) {
         fun step() {
             x += velocityX
             y += velocityY
 
             if (x >= width - radius || x <= radius) {
-                velocityX = -velocityX
+                bounceHorizontal()
             }
 
             if (y >= height - radius - paddleHeight) {
                 if (x > paddleX && x < paddleX + paddleWidth) {
-                    velocityY = -velocityY
+                    bounceVertical()
                 }
                 if (y >= height - radius) {
                     lives--
@@ -69,17 +87,63 @@ class BreakoutEngine(var width: Double, var height: Double, val ballRadius: Doub
                 }
             }
             if (y <= radius) {
-                velocityY = -velocityY
+                bounceVertical()
             }
+        }
+
+        fun bounceHorizontal() {
+            velocityX = -velocityX
+        }
+
+        fun bounceVertical() {
+            velocityY = -velocityY
         }
     }
 
     interface GameStateListener {
         fun ballMoved(x: Double, y: Double)
         fun paddleMoved(x: Double, y: Double)
+        fun blockUpdated(block: Block)
         fun ballMissedPaddle()
         fun numberOfLivesChanged(lives: Int)
         fun gameOver()
+    }
+
+    class Block(blockX: Double, blockY: Double, blockWidth: Double, blockHeight: Double, var blockState: BlockState) : Rectangle(blockX, blockY, blockWidth, blockHeight) {
+        var beingHit = false
+        fun checkHit(ball: Ball) {
+            println("hit = ${intersects(ball)} blockState = ${blockState.name}")
+            if (intersects(ball)) {
+                if (!beingHit) {
+                    if (blockState == BlockState.NEW) {
+                        blockState = BlockState.HIT
+                    } else if (blockState == BlockState.HIT) {
+                        blockState = BlockState.DESTROYED
+                    }
+                    beingHit = true;
+                }
+
+            } else {
+                beingHit = false
+            }
+        }
+
+        override fun toString(): String {
+            return "Block(x=$x, y=$y, gameWidth=$w, gameHeight=$h, blockState=$blockState)"
+        }
+
+    }
+
+    enum class BlockState {
+        NEW,
+        HIT,
+        DESTROYED
+    }
+}
+
+open class Rectangle(var x: Double, var y: Double, val w: Double, val h: Double) {
+    fun intersects(r: Rectangle): Boolean {
+        return x < r.x + r.w && x + w > r.x && y < r.y + r.h && y + h > r.y;
     }
 }
 
